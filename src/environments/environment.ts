e1cfbcd7b8c5523d9780c30e5f98cb412a7f3d74
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-06-24 15:48:35 -0700

    Fix API 4xx error #5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://j-store-psi.vercel.app',
+  apiUrl: 'https://j-store-backend.onrender.com',
   stripePublishableKey: "pk_test_51NMbmiJRfusv8EuT4rh94CKgLmaBwKqu8fApdaoH8OVwTWXpmY1b8rGmuLHMldTJ7WRMshRH4rRa3LS2TeOmj9eV005vUt79GW"
 };
